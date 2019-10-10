0c333d07aa26b66334a2b01fda9a70ea27c70a46
Johannes Lötzsch <git@johannesloetzsch.de>
2019-10-10 04:29:11 +0200

    added example

--- src/D3SvgOverlay.js
+++ src/D3SvgOverlay.js
@@ -2,23 +2,28 @@ import { MapLayer } from 'react-leaflet';
 import 'leaflet-d3-svg-overlay';
 
 export default class D3SvgOverlay extends MapLayer {
-
   createLeafletElement(props) {
-    const { layerContainer, map } = this.props.leaflet || this.context;
-    var { data, f } = this.props;
-
-    var overlay = L.d3SvgOverlay(function(sel, proj) {
-        f(sel, proj, data);
+    const { data, drawCallback } = this.props;
+    return L.d3SvgOverlay(function(sel, proj) {
+      drawCallback(sel, proj, data);
     });
-
-    overlay.addTo(layerContainer);
-    overlay.addTo(map);
   }
 
   componentDidMount() {
+    const { layerContainer, map } = this.props.leaflet || this.context;
+    this.leafletElement.addTo(layerContainer);
+    this.leafletElement.addTo(map);
   }
 
   componentWillUnmount() {
+    const { layerContainer, map } = this.props.leaflet || this.context;
+    map.removeLayer(this.leafletElement);
+    layerContainer.removeLayer(this.leafletElement);
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    this.componentWillUnmount();
+    this.leafletElement = this.createLeafletElement(this.props);
+    this.componentDidMount();
+  }
 }
